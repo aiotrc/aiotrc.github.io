50cae2cd72377e8fa3b8b38ead8994f337bfaacc
Parham Alvani <parham.alvani@gmail.com>
2019-02-11 23:03:31 +0330

    Nothing important.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 		// Cloning main navigation for mobile menu
 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
 
-		// Mobile menu toggle 
+		// Mobile menu toggle
 		$(".menu-toggle").click(function(){
 			$(".mobile-navigation").slideToggle();
 		});
